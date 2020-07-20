d4540b0086c8ef47554afaa478445ae05d91d44c
flucas97 <feijolucas1997@gmail.com>
2020-07-19 22:59:21 -0300

    perm check, 100%, O(N)

--- Codility/Counting/permutionArray.rb
+++ Codility/Counting/permutionArray.rb
@@ -1,24 +1,42 @@
-
 def solution(a)
- unless a.count == 0 
-   a.sort!
-   last = a.first
-   ok = 1
-   notOk = 0
-   
-   return 0 if a.count == 1 && a.first == 1
-   return 1 if a.count == 2 && a.first > 1 && a.last - a.first >= 2
-   return 0 if a.count == 2 && a.first >= 1 && a.last - a.first < 2
-   
-   a.each do |actual|
-      before = last
-      last = actual
+  ok = 1
+  notOk = 0
+  original = a.clone
+  
+  unless a.count == 0    
+      return notOk if a.first <= 0 && a.last <= 0
+      a.sort!.uniq!
+      return notOk if a.count != original.count 
+
+      if a.count == 1 
+          return ok if a.first == 1 && original.count == 1
+          return notOk if a.first == 1 && original >= 2
       
-      math =  actual - before
-      return notOk if math >= 2 || math <= -2
-   end
-   return ok
- else
-   return 0
- end
+          return notOk if a.first != 1  
+      elsif a.count == 2 
+          math = a.first - a.last
+          
+          return notOk if math >= 2 || math <= -2
+          return notOk if math == 1 && a.first > 1 || a.first < 0
+          return ok if math == 1 || math == -1 && a.first == 1
+          return notOk if math == 1 || math == -1 && a.first != 1
+      else
+          return notOk unless a.include?(1)
+          last = a.first
+          
+          a.each do |actual|
+              before = last
+              last = actual
+
+              next if last == -1 && actual == 1
+
+              math =  actual - before
+
+              return notOk if math >= 2 || math <= -2
+          end
+          return ok
+      end
+  else
+      return notOk
+  end
 end
