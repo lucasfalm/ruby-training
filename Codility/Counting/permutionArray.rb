1bedd88313db7c30d5c96c1a07b6c70ea1582ee5
flucas97 <feijolucas1997@gmail.com>
2020-07-19 23:01:39 -0300

    clean code

--- Codility/Counting/permutionArray.rb
+++ Codility/Counting/permutionArray.rb
@@ -9,17 +9,19 @@ def solution(a)
       return notOk if a.count != original.count 
 
       if a.count == 1 
-          return ok if a.first == 1 && original.count == 1
-          return notOk if a.first == 1 && original >= 2
-      
-          return notOk if a.first != 1  
+          if a.first == 1 && original.count == 1
+            return ok
+          else
+            return notOk
+          end
       elsif a.count == 2 
           math = a.first - a.last
           
-          return notOk if math >= 2 || math <= -2
-          return notOk if math == 1 && a.first > 1 || a.first < 0
-          return ok if math == 1 || math == -1 && a.first == 1
-          return notOk if math == 1 || math == -1 && a.first != 1
+          if math == 1 || math == -1 && a.first == 1
+            return ok
+          else
+            return notOk
+          end
       else
           return notOk unless a.include?(1)
           last = a.first
