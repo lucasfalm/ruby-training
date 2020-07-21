a65044ef1e3349e9d035b75f8fa5922e3a285791
flucas97 <feijolucas1997@gmail.com>
2020-07-20 22:17:27 -0300

    Big O(n)

--- Codility/Counting/isRotation.rb
+++ Codility/Counting/isRotation.rb
@@ -1,24 +1,23 @@
 
-arrayOne = [1,2,3,4,5]
-arrayTwo = [3,4,5,1,6]
+arrayOne = [1, 2, 3, 4, 5, 6, 7]
+arrayTwo = [4, 5, 6, 7, 1, 2, 3]
 
 is_rotation(arrayOne, arrayTwo)
 
-# Big O(n + m)
+# Big O(n)
 def is_rotation(arrayOne, arrayTwo)
     maxLength = arrayOne.count - 1
     p1 = 0
     p2 = maxLength
 
     while p1 <= maxLength && p2 >= 0 do
-        puts "entrou"
         if arrayOne[p1] == arrayTwo[p2]
             p1 += 1
             p2 = maxLength
         else
             p2 -= 1
         end
-        puts p2 if p2 == 0
+        return false if p2 < 0
     end
 
     return true
