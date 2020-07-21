5a5824f124161b4a6dff57c0ed1edd3e0a61a58f
flucas97 <feijolucas1997@gmail.com>
2020-07-20 21:18:40 -0300

    refactor for Big O(n+m)

--- Codility/Counting/commomElements.rb
+++ Codility/Counting/commomElements.rb
@@ -1,19 +1,26 @@
 
-a = [1,2,3,4,5]
-b = [3,4,5,6,7,8,9,10]
+arrayOne = [1,2,3,4,5]
+arrayTwo = [3,4,5,6,7,8,9,10]
 
-commom_elements(a, b)
+commom_elements(arrayOne, arrayTwo)
 
+# Big O(n, m)
 def commom_elements(arrayOne, arrayTwo)
-    hashTable = Hash.new(0)
+    p1 = 0
+    p2 = 0
     commomElements = Array.new()
+    maxOne = arrayOne.count - 1
+    maxTwo = arrayTwo.count - 1
 
-    arrayOne.each do |element|
-        hashTable[element] += 1
-    end
-
-    arrayTwo.each do |element|
-        commomElements << element if hashTable[element]
+    while p1 <= maxOne && p2 <= maxTwo do
+        if arrayOne[p1] == arrayTwo[p2]
+            commomElements << arrayOne[p1]
+            p1 += 1
+        elsif arrayOne[p1] < arrayTwo[p2]
+            p1 += 1
+        else
+            p2 += 1
+        end
     end
 
     commomElements
