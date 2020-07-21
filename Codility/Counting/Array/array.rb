5aed657d1e84d4706cbce92a8d07ce0b8b420fcb
flucas97 <feijolucas1997@gmail.com>
2020-07-21 17:13:09 -0300

    create class Array, for trainning, with time complexity

---
+++ Codility/Counting/Array/array.rb
@@ -0,0 +1,44 @@
+class MyArray
+    def initialize(size)
+        @size = size
+        setBackingArray()
+    end
+
+    def setBackingArray()
+        @selfArray = Array.new(@size) # O(1)
+    end
+
+    def getAtIndex(index)
+        @selfArray[index] # O(1)
+    end
+
+    def showAll()
+        @selfArray # O(1)
+    end
+
+    def setValueAtIndex(index, value)
+        @selfArray[index] = value # O(1)
+    end
+
+    def insertAtAll(value)
+        @selfArray = @selfArray.map { |v| v = value } # O(n)
+    end
+
+    def insertAtIndex(index, value)
+        @selfArray.insert(index, value) # O(n)
+    end
+
+    def insertAtEnd(value)
+        @selfArray << value # O(1)
+    end
+
+    def insertAtFirst(value)
+        @selfArray.unshift(value) # O(1)
+    end
+
+    def deleteAtIndex(index)
+        @selfArray.delete(index) # O(n)
+    end
+end
+
+myArray = MyArray.new(5)
