1f93172b64e0e5974f528451d9e0aa384ee65b92
flucas97 <feijolucas1997@gmail.com>
2020-07-23 02:45:23 -0300

    distinct in array, O(n log n) OR O(n)

---
+++ Codility/DataStructures/Array/distinctInArray.rb
@@ -0,0 +1,12 @@
+def solution(a)
+    unless a.count == 0
+        return 1 if a.count == 1
+        a.uniq! if a.count != a.uniq! 
+        return a.count
+    end
+   return 0
+end
+
+
+a = [1,2,2,3,4,5,6,6,5,4,3,2,1,2]
+solution(a)
