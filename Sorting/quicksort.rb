8c9f568dc58c581411b498a9f42f19cdf1a122fc
flucas97 <feijolucas1997@gmail.com>
2020-07-23 14:57:28 -0300

    implement quick sort, Big O(n log n)

---
+++ Sorting/quicksort.rb
@@ -0,0 +1,19 @@
+class QuickSort
+    def sort(array)
+
+        quickSort(array, 0, array.length - 1)
+
+        return array
+    end
+
+    def quickSort(array, left, right)
+        return if (left >= right)
+        
+        pivot = arra[(left + right) / 2]
+    end
+end
+
+class Node
+
+
+end
