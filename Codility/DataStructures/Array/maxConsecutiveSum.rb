d003aa709a92a19dc961c40bcbcbfd8b6612f6b8
flucas97 <feijolucas1997@gmail.com>
2020-07-25 00:43:07 -0300

    clean code

--- Codility/DataStructures/Array/maxConsecutiveSum.rb
+++ Codility/DataStructures/Array/maxConsecutiveSum.rb
@@ -1,15 +1,16 @@
 def arrayMaxConsecutiveSum2(inputArray)
     max_so_far_evaluated = 0
     max_for_each_iteration = 0
-        inputArray.each do |element|
-            max_for_each_iteration = max_for_each_iteration + element
-            if(max_for_each_iteration < 0)
-                max_for_each_iteration = 0
-            end
-            if(max_so_far_evaluated < max_for_each_iteration)
-                max_so_far_evaluated = max_for_each_iteration
-            end
+    
+    inputArray.each do |element|
+        max_for_each_iteration = max_for_each_iteration + element
+        if(max_for_each_iteration < 0)
+            max_for_each_iteration = 0
         end
+        if(max_so_far_evaluated < max_for_each_iteration)
+            max_so_far_evaluated = max_for_each_iteration
+        end
+    end
 
     return -1 if max_so_far_evaluated == 0
     max_so_far_evaluated
