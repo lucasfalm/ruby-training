90c12d20112d2c3bd5dbe2237e33151c32753134
flucas97 <feijolucas1997@gmail.com>
2020-07-25 01:25:44 -0300

    clean code.

--- Codility/DataStructures/Array/maxSubArrayPosition.rb
+++ Codility/DataStructures/Array/maxSubArrayPosition.rb
@@ -6,20 +6,27 @@ def findLongestSubarrayBySum(s, arr)
     p0 = 0
     p1 = 0
     indexes_of_so_far_evaluated = Array.new(0)
-    
-    while p1 <= arr.count 
+    last_current_value = 0
+
+    while p1 <= arr.count
+        last_current_value = max_for_each_iteration
         max_for_each_iteration += arr[p1].to_i
         indexes_of_so_far_evaluated << p1 + 1
+
         if max_for_each_iteration == s
             if indexes_of_so_far_evaluated.count != 2
                 start = indexes_of_so_far_evaluated.first
                 ends = indexes_of_so_far_evaluated.last
+            
                 return [start, ends]
             end
+
             return indexes_of_so_far_evaluated
-        elsif max_for_each_iteration < s
+
+        elsif max_for_each_iteration < s && max_for_each_iteration >= last_current_value
             p1 += 1
-        elsif max_for_each_iteration > s
+
+        elsif max_for_each_iteration > s || max_for_each_iteration <= last_current_value
             max_so_far_evaluated = max_for_each_iteration
             p0 += 1
             p1 -= p0
