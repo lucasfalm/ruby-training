19c5c0590313647bdebcd77f6b0bf1ee554ac5ce
flucas97 <feijolucas1997@gmail.com>
2020-07-25 02:14:24 -0300

    prision after n days

---
+++ Codility/DataStructures/Array/prisionAfterNDays.rb
@@ -0,0 +1,47 @@
+def prison_after_n_days(cells, n)
+    daysLeft = 0
+    
+    while daysLeft <= n
+        cells = calculate(cells, n)
+        daysLeft += 1
+    end
+    
+    return cells
+end
+
+def calculate(cells, n) 
+    nextState = Array.new(0)
+    p0 = 0
+    pleft = p0 - 1
+    pright = p0 + 1
+    
+    i = 0 
+    while i <= cells.count - 1
+        if pleft < 0
+            nextState[p0] = 0
+        elsif pright >= cells.count - 1
+            nextState[p0] = 0
+        elsif cells[pleft] != cells[pright]
+            nextState[p0] = 1
+        elsif cells[pleft] == cells[pright]
+            nextState[p0] = 0
+        end
+        p0 += 1
+        pleft = p0 - 1
+        pright = p0 + 1
+        
+        i += 1 
+    end
+    y = 0
+
+    while y <= nextState.count - 1
+        cells[y] = nextState[y]
+        y += 1
+    end
+
+    p0 = 0
+    pleft = p0 - 1
+    pright = p0 + 1
+  
+    return cells
+end
