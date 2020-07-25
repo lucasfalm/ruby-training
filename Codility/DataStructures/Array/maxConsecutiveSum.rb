cfde25d2bd34776c097005fac60a08dc17411716
flucas97 <feijolucas1997@gmail.com>
2020-07-25 00:15:22 -0300

    refactor Big O(N) instead Big O(N + M) time complexity

--- Codility/DataStructures/Array/maxConsecutiveSum.rb
+++ Codility/DataStructures/Array/maxConsecutiveSum.rb
@@ -1,32 +1,22 @@
 def arrayMaxConsecutiveSum2(inputArray)
-    p0 = 0
-    maxConsecutive = Array.new(0)
-    maxResult = 0
-    
-    while p0 <= inputArray.count
-        current = inputArray[p0]
-        if current.to_i >= 0
-            maxConsecutive << current.to_i
-        else
-            maxSum = 0
-            i = 0
-            while i <= maxConsecutive.count
-                maxSum += maxConsecutive[i].to_i
-                i += 1
-            end
-
-            if maxSum == 0 
-                maxResult = maxSum
-            end
-            if maxSum > maxResult
-                maxResult = maxSum
-            end
-            
-            maxConsecutive = Array.new(0)
-        end
-        
-        p0 += 1
-    end
-    return -1 if maxResult <= 0
-    return maxResult
-end
+    maxSum = 0
+    actualResult = 0
+    size = inputArray.count
+    i = 0
+ 
+     while i <= size
+         current = inputArray.shift
+         if current.to_i <= 0
+             if actualResult > maxSum
+                 maxSum = actualResult
+             end
+             actualResult = 0
+         else
+             actualResult += current.to_i
+         end 
+         i += 1
+     end 
+     return -1 if maxSum <= 0
+     return maxSum
+ end
+ 
