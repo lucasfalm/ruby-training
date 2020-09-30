2109a92c75c84c09e809b5a66e97318305f288e0
Lucas Feijo <feijolucas1997@gmail.com>
2020-09-30 16:17:16 -0300

    minimum distance challange

---
+++ challanges/data-structures/array/minimum-distance.rb
@@ -0,0 +1,57 @@
+require 'json'
+require 'stringio'
+# https://www.hackerrank.com/challenges/minimum-distances/problem
+# Complete the minimumDistances function below.
+def minimumDistances(a)
+    array_size = a.count - 1
+    
+    return -1 if array_size < 1
+
+    if array_size == 1
+        if a[0] == a[1]
+            return 1
+        end
+
+        return -1
+    end
+
+    min = 10 * 10 ** 10
+    start_pointer = 0
+    end_pointer = array_size
+
+    while start_pointer <= array_size do
+        if a[start_pointer] == a[end_pointer]
+            distance = (start_pointer - end_pointer).abs    
+    
+            if distance < min
+                min = distance if distance != 0
+            end
+
+            start_pointer += 1
+            end_pointer = start_pointer + 1
+        else 
+            end_pointer += 1
+        end
+
+        if end_pointer > array_size
+            start_pointer += 1
+            end_pointer = start_pointer + 1
+        end 
+    end
+
+    return -1 if min == 10 * 10 ** 10
+    min
+end
+
+fptr = File.open(ENV['OUTPUT_PATH'], 'w')
+
+n = gets.to_i
+
+a = gets.rstrip.split(' ').map(&:to_i)
+
+result = minimumDistances a
+
+fptr.write result
+fptr.write "\n"
+
+fptr.close()
