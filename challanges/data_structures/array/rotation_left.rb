84dbf2e8ee1fda06d9a8ffd574446128e95b4071
Lucas Feijo <feijolucas1997@gmail.com>
2021-11-07 17:48:38 -0300

    :zap: chore: remove useless file from recursion

--- challanges/data_structures/array/rotation_left.rb
+++ challanges/data_structures/array/rotation_left.rb
@@ -1,11 +1,11 @@
 # Big O(n) time and O(n) space
-def rotLeft(a, d)
+def rotate_left(a, d)
     counter = 1
 
     while counter <= d
         last = a.shift
         a.push(last)
-    
+
         counter += 1
     end
 
