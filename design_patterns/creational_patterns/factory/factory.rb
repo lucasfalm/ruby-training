55eb68f0292f0b011cfccfc62b549f8c8eafabc3
Lucas Feijo <feijolucas1997@gmail.com>
2021-11-16 19:06:50 -0300

    :fire: feat: studies about Factory design patterns, including Factory method and just Factory for now. Soon I'll write about Abstract Factory

---
+++ design_patterns/creational_patterns/factory/factory.rb
@@ -0,0 +1,40 @@
+# NOTE: the Factory design pattern is basically a separated class
+#       that is responsible solely for the wholesale (not piecewise)
+#       creation of objects.
+
+class Point
+
+  def initialize x, y
+    @x = x
+    @y = y
+  end
+
+  # NOTE: exposing the factory
+  # 
+  def self.factory
+    PointFactory
+  end
+end
+
+class PointFactory
+
+  def self.new_cartesian_point x:, y:
+    Point.new x, y
+  end
+
+  def self.new_solar_point rho:, theta:
+    Point.new(
+      rho * Math.cos(theta),
+      rho * Math.sin(theta)
+    )
+  end
+end
+
+# NOTE: we use the factory class to create the object
+#       but it's pretty much the Factory design pattern, but following
+#       the separation of concerns.
+# 
+cartesian_point = Point.factory.new_cartesian_point x: 50, y: 40
+solar_point = Point.factory.new_solar_point rho: 50, theta: 40
+
+puts cartesian_point.inspect
---
+++ design_patterns/creational_patterns/factory/factory_method.rb
@@ -0,0 +1,32 @@
+require "math"
+
+# NOTE: the Factory method design pattern is basically about
+#       methods inside of a class, that are responsible
+#       for creating the wholesale (not piecewise) objects.
+
+class Point
+
+  def initialize x, y
+    @x = x
+    @y = y
+  end
+
+  # NOTE: this method is a factory method, it return
+  #       an instance of Point, representing the cartesian point
+  # 
+  def self.new_cartesian_point x: y:
+    self.new x, y
+  end
+
+  def self.new_solar_point rho:, theta:
+    self.new(
+      rho * Math.cos(theta),
+      rho * Math.sin(theta)
+    )
+  end
+end
+
+# NOTE: the factory method is inside of the object itself.
+# 
+cartesian_point = Point.new_cartesian_point x: 50, y: 40
+solar_point = Point.new_solar_point rho: 50, theta: 40
