3dc733f9943eb8e1decc91cf7e5e851489ed36df
Lucas Feijo <feijolucas1997@gmail.com>
2021-11-19 19:39:01 -0300

    :sparkles: feat: add Singleton design pattern (or anti), under creational patterns

---
+++ design_patterns/creational_patterns/singleton/singleton.rb
@@ -0,0 +1,22 @@
+# NOTE: well... the Singleton design pattern (is also considered an anti-pattern)
+#       and it's bascially about having only one instance in the entire system
+# 
+
+require 'singleton'
+
+class Foobar
+    include Singleton
+end
+
+# NOTE: it raises an error
+# 
+# foo = Foobar.new
+#       
+#       when using the Singleton module from Ruby, you make the 'new' a private method
+#       and you have access only to a method called 'instance'
+# 
+bar = Foobar.instance
+foo = Foobar.instance
+
+puts foo === bar
+# true
