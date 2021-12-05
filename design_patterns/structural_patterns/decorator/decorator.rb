00a49d8d07b00c42f98c5da532297557675e005c
Lucas Feijo <feijolucas1997@gmail.com>
2021-12-05 01:33:32 -0300

    :art: chore: improve comments on decorator

--- design_patterns/structural_patterns/decorator/decorator.rb
+++ design_patterns/structural_patterns/decorator/decorator.rb
@@ -8,5 +8,5 @@ game_example = Games::Example.new
 game_example.next_round
 
 # Game::Base#next_round with , {}
-# Game::TriggerSpin#next_round with , {}
-# Game::Features::StickySymbol#next_round with , {}
+# Game::TriggerSpin#next_round with , {:spin=>true}
+# Game::Features::StickySymbol#next_round with , {:spin=>true, :sticky_simbols=>[0, 1, 5, 10]}
